f0b3ec088db9200963e4269352d083c71ba10679
xyz <gky44px1999@gmail.com>
2021-05-13 23:38:05 -0700

    better dwm status bar

--- programs/my_scripts/set_dwm_status_bar.sh
+++ programs/my_scripts/set_dwm_status_bar.sh
@@ -1,4 +1,7 @@
 while true; do
-	xsetroot -name "$(date "+%a %m/%d/%Y %H:%M:%S") | $(cat /sys/class/power_supply/BAT1/capacity)%"
+	xsetroot -name "$(date "+%a %m/%d/%Y %H:%M:%S") \
+| $(cat /sys/class/net/wlp6s0/operstate) \
+| $(if xset q | grep -q "Caps Lock: *on"; then echo A; else echo a; fi) \
+| $(cat /sys/class/power_supply/BAT1/capacity)%"
 	sleep 1
 done
