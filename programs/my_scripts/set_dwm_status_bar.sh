2a2cc1ec624694a55b900918f7da675047800e48
xyz <gky44px1999@gmail.com>
2021-05-18 04:23:31 -0700

    less time info on status bar

--- programs/my_scripts/set_dwm_status_bar.sh
+++ programs/my_scripts/set_dwm_status_bar.sh
@@ -1,5 +1,5 @@
 while true; do
-	xsetroot -name "$(date "+%a %m/%d/%Y %H:%M:%S") \
+	xsetroot -name "$(date "+%a %m/%d %H:%M") \
 | $(cat /sys/class/net/wlp6s0/operstate) \
 | $(if xset q | grep -q "Caps Lock: *on"; then echo A; else echo a; fi) \
 | $(cat /sys/class/power_supply/BAT1/capacity)%"
