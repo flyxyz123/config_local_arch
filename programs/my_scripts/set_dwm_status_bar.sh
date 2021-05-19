2951651842c8fa3e08fc85328d04382062f9b759
xyz <gky44px1999@gmail.com>
2021-05-19 00:22:01 -0700

    add CPU temp and free memory to status bar

--- programs/my_scripts/set_dwm_status_bar.sh
+++ programs/my_scripts/set_dwm_status_bar.sh
@@ -1,7 +1,9 @@
 while true; do
 	xsetroot -name "$(date "+%a %m/%d %H:%M") \
-| $(cat /sys/class/net/wlp6s0/operstate) \
 | $(if xset q | grep -q "Caps Lock: *on"; then echo A; else echo a; fi) \
+| $(cat /sys/class/net/wlp6s0/operstate) \
+| $(free -h | awk '(NR==2){ print $4 }') \
+| $(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp) \
 | $(cat /sys/class/power_supply/BAT1/capacity)%"
 	sleep 1
 done
