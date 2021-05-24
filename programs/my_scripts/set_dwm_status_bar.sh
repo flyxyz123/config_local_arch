f8b801b9b73fde21dd708960798b53dc1b684ab8
xyz <gky44px1999@gmail.com>
2021-05-23 18:32:27 -0700

    add desktop user dir, while loop time sleep in .xinitrc

--- programs/my_scripts/set_dwm_status_bar.sh
+++ programs/my_scripts/set_dwm_status_bar.sh
@@ -1,9 +1,6 @@
-while true; do
-	xsetroot -name "$(date "+%a %m/%d %H:%M") \
+xsetroot -name "$(date "+%a %m/%d %H:%M") \
 | $(if xset q | grep -q "Caps Lock: *on"; then echo A; else echo a; fi) \
 | $(cat /sys/class/net/wlp6s0/operstate) \
 | $(free -h | awk '(NR==2){ print $4 }') \
 | $(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp) \
 | $(cat /sys/class/power_supply/BAT1/capacity)%"
-	sleep 1
-done
