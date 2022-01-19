58bae6f67fae08fb3c028abb3226d88436315f23
xyz <gky44px1999@gmail.com>
2022-01-18 16:33:01 -0800

    refactor

--- home/xyz/.mozilla/firefox/xxxxxxxx.fly/user-overrides.js
+++ home/xyz/.mozilla/firefox/xxxxxxxx.fly/user-overrides.js
@@ -1,5 +1,5 @@
 // fixes
-// a lot: image upload from local...
+// a lot: image upload from local, image check not robot
 //user_pref("privacy.resistFingerprinting", false);
 // douyu pay, bilibili stream watch and user space, pixiv some images not show
 user_pref("network.http.referer.XOriginPolicy", 0);
